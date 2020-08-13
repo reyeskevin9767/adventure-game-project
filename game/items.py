72cc680cbef09bdcfe3a8e7ad46aa7e2472faf13
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-12 22:02:19 -0400

    add new items and tests

--- game/items.py
+++ game/items.py
@@ -1,6 +1,5 @@
 class Item():
     """ The base information for all items """
-    # Constructor
 
     def __init__(self, name, description, value):
         self.name = name
@@ -12,10 +11,81 @@ class Item():
 
 
 class Gold(Item):
-    """ Subclass of Item """
+    """ Info About Gold """
+
     def __init__(self, amount):
         self.amount = amount
         super().__init__(name="Gold",
-                         description=f"A Round Coin With {self.amount} stamped on the front.",
+                         description=f"A round coin with {self.amount} stamped on the front.",
                          value=self.amount)
 
+
+class Crystals(Item):
+    """ Info About Crystals """
+
+    def __init__(self, amount):
+        self.amount = amount
+        super().__init__(name="Crystals",
+                         description=f"A jagged rock with sharp edges worth {self.amount}.",
+                         value=self.amount)
+
+
+class Weapon(Item):
+    """ The base information for all weapons """
+
+    def __init__(self, name, description, value, damage):
+        self.damage = damage
+        super().__init__(name, description, value)
+
+        def __str__(self):
+            return f"{self.name}\n=====\nValue: {self.value}\nDamage: {self.damage}"
+
+
+class Gloves(Weapon):
+    """ Info About Gloves """
+
+    def __init__(self):
+        super().__init__(name="Gloves",
+                         description="Regular pair of gloves. All its power comes from you.",
+                         value=0,
+                         damage=5)
+
+
+class Sword(Weapon):
+    """ Info About Sword """
+
+    def __init__(self):
+        super().__init__(name="Wood Sword",
+                         description="A sword typically used for training. Better than using your fists.",
+                         value=5,
+                         damage=10)
+
+
+class Dragger(Weapon):
+    """ Info About Dragger """
+
+    def __init__(self):
+        super().__init__(name="Rusty Dragger",
+                         description="A small dagger with some rust. Be better if you could throw the dragger.",
+                         value=10,
+                         damage=15)
+
+
+class Axe(Weapon):
+    """ Info About Axe """
+
+    def __init__(self):
+        super().__init__(name="Axe of Echo",
+                         description="A weapon abandoned by a famous adventure. Pretty good axe.",
+                         value=15,
+                         damage=25)
+
+
+class NightSword(Weapon):
+    """ Info About NightSword """
+
+    def __init__(self):
+        super().__init__(name="Night's Edge",
+                         description="A black sword with a snake style blade that deals deadly damage.",
+                         value=25,
+                         damage=35)
--- tests/test_items.py
+++ tests/test_items.py
@@ -1,21 +1,118 @@
 import unittest
-from game.items import Item, Gold
+from game.items import Item, Gold, Crystals, Weapon, Gloves, Sword, Dragger, Axe, NightSword
 
 # * Testing Items
+
+
 class BaseItemCases:
     class ItemTests(unittest.TestCase):
+        """ Testing Item Template """
+
         def test_str(self):
             self.assertIsNotNone(str(self))
 
 
 class GoldTests(BaseItemCases.ItemTests):
+    """ Testing Gold Information And Amount """
+
     def setUp(self):
         self.gold = Gold(3)
 
     def test_init(self):
         self.assertEqual(self.gold.name, 'Gold')
-        self.assertEqual(self.gold.description, "A Round Coin With 3 stamped on the front.")
+        self.assertEqual(self.gold.description,
+                         "A round coin with 3 stamped on the front.")
         self.assertEqual(self.gold.value, 3)
+
+
+class CrystalsTests(BaseItemCases.ItemTests):
+    """ Testing Crystals Information And Amount """
+
+    def setUp(self):
+        self.crystals = Crystals(1)
+
+    def test_init(self):
+        self.assertEqual(self.crystals.name, 'Crystals')
+        self.assertEqual(self.crystals.description,
+                         "A jagged rock with sharp edges worth 1.")
+        self.assertEqual(self.crystals.value, 1)
+
+
+class WeaponTests(BaseItemCases.ItemTests):
+    """ Testing Weapon Template """
+
+    def test_str(self):
+        self.assertIsNotNone(str(self))
+
+
+class GlovesWeaponTests(WeaponTests):
+    """ Test Gloves Information And Damage """
+
+    def setUp(self):
+        self.gloves = Gloves()
+
+    def test_init(self):
+        self.assertEqual(self.gloves.name, 'Gloves')
+        self.assertEqual(self.gloves.description,
+                         'Regular pair of gloves. All its power comes from you.')
+        self.assertEqual(self.gloves.value, 0)
+        self.assertEqual(self.gloves.damage, 5)
+
+
+class SwordWeaponTests(WeaponTests):
+    """ Test Sword Information And Damage """
+
+    def setUp(self):
+        self.sword = Sword()
+
+    def test_init(self):
+        self.assertEqual(self.sword.name, 'Wood Sword')
+        self.assertEqual(self.sword.description,
+                         'A sword typically used for training. Better than using your fists.')
+        self.assertEqual(self.sword.value, 5)
+        self.assertEqual(self.sword.damage, 10)
+
+
+class DraggerWeaponTests(WeaponTests):
+    """ Test Dagger Information And Damage """
+
+    def setUp(self):
+        self.dragger = Dragger()
+
+    def test_init(self):
+        self.assertEqual(self.dragger.name, 'Rusty Dragger')
+        self.assertEqual(self.dragger.description,
+                         'A small dagger with some rust. Be better if you could throw the dragger.')
+        self.assertEqual(self.dragger.value, 10)
+        self.assertEqual(self.dragger.damage, 15)
+
+
+class AxeWeaponTests(WeaponTests):
+    """ Test Axe Information And Damage """
+
+    def setUp(self):
+        self.axe = Axe()
+
+    def test_init(self):
+        self.assertEqual(self.axe.name, 'Axe of Echo')
+        self.assertEqual(self.axe.description,
+                         'A weapon abandoned by a famous adventure. Pretty good axe.')
+        self.assertEqual(self.axe.value, 15)
+        self.assertEqual(self.axe.damage, 25)
+
+
+class NightSwordWeaponTests(WeaponTests):
+    """ Test Axe Information And Damage """
+
+    def setUp(self):
+        self.night_sword = NightSword()
+
+    def test_init(self):
+        self.assertEqual(self.night_sword.name, "Night's Edge")
+        self.assertEqual(self.night_sword.description,
+                         'A black sword with a snake style blade that deals deadly damage.')
+        self.assertEqual(self.night_sword.value, 25)
+        self.assertEqual(self.night_sword.damage, 35)
 
 
 # python -m unittest tests/test_items.py
